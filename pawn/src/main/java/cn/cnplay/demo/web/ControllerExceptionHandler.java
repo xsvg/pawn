adbcae984b342c8fddd36101d6e09be22022d411
icoop <icoop@icoop-PC>
2018-08-10 11:28:13 +0800

    增加未登录和无权限异常的处理

--- pawn/src/main/java/cn/cnplay/demo/web/ControllerExceptionHandler.java
+++ pawn/src/main/java/cn/cnplay/demo/web/ControllerExceptionHandler.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+import cn.cnplay.demo.exception.UnAuthorizedException;
+import cn.cnplay.demo.exception.UnLoginException;
 import cn.cnplay.demo.utils.ResultVOUtil;
 import cn.cnplay.demo.vo.ResultVo;
 
@@ -15,6 +17,20 @@ import cn.cnplay.demo.vo.ResultVo;
 @ResponseBody
 public class ControllerExceptionHandler {
 	private final static Logger LOG = LoggerFactory.getLogger(ControllerExceptionHandler.class);
+	
+	@ExceptionHandler(UnAuthorizedException.class)
+	@ResponseStatus(HttpStatus.FORBIDDEN)
+	public ResultVo<?> handleUnAuthorizedException(UnAuthorizedException e){
+		LOG.warn("无权限",e);
+		return ResultVOUtil.error(e.getMessage());
+	}
+	
+	@ExceptionHandler(UnLoginException.class)
+	@ResponseStatus(HttpStatus.UNAUTHORIZED)
+	public ResultVo<?> handleUnLoginExceptionn(UnLoginException e){
+		LOG.warn("未登录或已过期",e);
+		return ResultVOUtil.error(e.getMessage());
+	}
 	
 	
 	@ExceptionHandler(Exception.class)
